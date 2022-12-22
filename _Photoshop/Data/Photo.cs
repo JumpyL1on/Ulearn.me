991f6afd7d86d7c88cbe7aa20a11deda5dc16568
Grigoriev Yakov <yaschok315@gmail.com>
2022-12-22 10:17:20 +0300

    Индексаторы - решение

--- _Photoshop/Data/Photo.cs
+++ _Photoshop/Data/Photo.cs
@@ -4,20 +4,25 @@ namespace MyPhotoshop
 	{
 		public int Width { get; }
 		public int Height { get; }
-		public Pixel[,] Data { get; }
+		private readonly Pixel[,] data;
 
 		public Photo(int width, int height)
 		{
 			Width = width;
 			Height = height;
-			Data = new Pixel[Width, Height];
-            for (var x = 0; x < Width; x++)
-            {
-                for (var y = 0; y < Height; y++)
-                {
-                    Data[x, y] = new Pixel();
-                }
-            }
-        }
+			data = new Pixel[Width, Height];
+			for (var x = 0; x < Width; x++)
+			{
+				for (var y = 0; y < Height; y++)
+				{
+					data[x, y] = new Pixel();
+				}
+			}
+		}
+
+		public Pixel this[int x, int y]
+		{
+			get { return data[x, y]; }
+		}
 	}
 }
--- _Photoshop/Filters/LighteningFilter.cs
+++ _Photoshop/Filters/LighteningFilter.cs
@@ -23,9 +23,9 @@ namespace MyPhotoshop
             for (int x = 0; x < result.Width; x++)
 				for (int y = 0; y < result.Height; y++)
 				{
-                    result.Data[x, y].R = original.Data[x, y].R * parameters[0];
-                    result.Data[x, y].G = original.Data[x, y].G * parameters[0];
-                    result.Data[x, y].B = original.Data[x, y].B * parameters[0];
+                    result[x, y].R = original[x, y].R * parameters[0];
+                    result[x, y].G = original[x, y].G * parameters[0];
+                    result[x, y].B = original[x, y].B * parameters[0];
                 }
 			return result;
 		}
