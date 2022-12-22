9735b9cfa3a6c58933ec87462b0b7f784f173500
Grigoriev Yakov <yaschok315@gmail.com>
2022-12-22 11:22:27 +0300

    Реализация интерфейса - решение

---
+++ _Photoshop/Filters/GrayscaleFilter.cs
@@ -0,0 +1,34 @@
+﻿namespace MyPhotoshop
+{
+    public class GrayscaleFilter : IFilter
+    {
+        public ParameterInfo[] GetParameters()
+        {
+            return new ParameterInfo[0];
+        }
+
+        public Photo Process(Photo original, double[] parameters)
+        {
+            var result = new Photo(original.Width, original.Height);
+
+            for (var x = 0; x < original.Width; x++)
+            {
+                for (var y = 0; y < original.Height; y++)
+                {
+                    var pixel = original[x, y];
+
+                    var average = (pixel.R + pixel.G + pixel.B) / 3;
+
+                    result[x, y] = new Pixel(average, average, average);
+                }
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return "Оттенки серого";
+        }
+    }
+}
